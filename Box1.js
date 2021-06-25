f75b2fdc068a8cf42bcba60f48b2b5f0e711bee0
ArionS35 <79088027+ArionS35@users.noreply.github.com>
2021-06-26 01:33:42 +0530

    Add files via upload

---
+++ Box1.js
@@ -0,0 +1,27 @@
+class Box {
+  constructor(x, y, width, height) {
+    var options = {
+        'restitution':0.6,
+        'friction':0.2,
+        'density':20,
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    this.image=loadImage("alien_block.png");
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    strokeWeight(2);
+    stroke("black");
+    fill("red");
+    image(this.image,0, 0, this.width, this.height);
+    pop();
+  }
+}
